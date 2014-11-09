8e89e2b4b4ef9ee87aa2eb591edb348b6c96d0d7
Anthony Johnston <aj@antix.co.uk>
2014-11-09 00:15:49 +0000

    missed files

---
+++ src/Antix.NuGet.Server/client/packages/packages-module.js
@@ -0,0 +1,64 @@
+﻿'use strict';
+
+angular.module('packages', [
+    'ngResource', 'ngSanitize'
+])
+
+    .directive('packages',
+    [
+        function () {
+
+            return {
+                restrict: 'E',
+                replace: true,
+                templateUrl: 'client/packages/packages.cshtml',
+                link: function () {
+
+                },
+                controller: 'PackagesController'
+            };
+        }
+    ])
+
+    .filter('packageDependencies',
+    [
+        function () {
+
+            return function (dependenciesString) {
+
+                return "<li>" +
+                    dependenciesString
+                    .replace(/:/g, ' ')
+                    .split('|').join('</li><li>')
+                    + "</li>";
+            }
+        }
+    ])
+
+    .controller('PackagesController',
+    [
+        '$scope',
+        'PackagesService',
+        function (
+            $scope,
+            PackagesService) {
+
+            PackagesService.get(function (response) {
+                $scope.packages = response.packages;
+            });
+        }
+    ])
+
+    .factory('PackagesService',
+    [
+        '$resource',
+        function ($resource) {
+
+            return $resource('', {}, {
+                get: {
+                    url: '/feed/Packages()',
+                    method: 'GET'
+                }
+            });
+        }
+    ]);
