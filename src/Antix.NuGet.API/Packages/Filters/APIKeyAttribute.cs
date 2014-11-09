b2fa796229dabec11f42d18194e232765ceab855
Anthony Johnston <aj@antix.co.uk>
2014-11-09 21:13:52 +0000

    missed file

---
+++ src/Antix.NuGet.API/Packages/Filters/APIKeyAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Antix.Http.Filters;
+
+namespace Antix.NuGet.API.Packages.Filters
+{
+    [AttributeUsage(
+        AttributeTargets.Class | AttributeTargets.Method,
+        AllowMultiple = false, Inherited = true)]
+    public class APIKeyAttribute :
+        Attribute, IFilterServiceAttribute
+    {
+        readonly Type _serviceType;
+
+        public APIKeyAttribute()
+        {
+            _serviceType = typeof (APIKeyFilter);
+        }
+
+        public Type ServiceType
+        {
+            get { return _serviceType; }
+        }
+    }
+}
