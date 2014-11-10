84e45404ba470f50dcd0c9fb96f3e57b78f592c8
Anthony Johnston <aj@antix.co.uk>
2014-11-10 00:34:57 +0000

    some header styling

--- src/Antix.NuGet.Server/client/home/home-module.js
+++ src/Antix.NuGet.Server/client/home/home-module.js
@@ -3,14 +3,43 @@
 angular.module('home', [
         'ngCookies',
         'packages'
-])
+    ])
     .controller(
         'HomeController',
         [
             '$scope',
-            function (
+            function(
                 $scope) {
 
 
             }
-        ]);
+        ])
+    .directive('scrollTop',
+    [
+        '$window', '$document',
+        function(
+            $window, $document) {
+
+            return {
+                restrict: 'A',
+                replace: false,
+                link: function(scope, element, attrs) {
+
+                    var scrollTop = function() {
+                        return ($window.pageYOffset !== undefined)
+                            ? $window.pageYOffset
+                            : ($document.documentElement || $document.body.parentNode || $document.body).scrollTop;
+                    }
+
+                    var target = angular.element($window),
+                        handler = function() {
+                            
+                            element.attr({ 'scroll-top': scrollTop() });
+                        };
+
+                    target.on('scroll', scope.$apply.bind(scope, handler));
+
+                }
+            };
+        }
+    ]);
